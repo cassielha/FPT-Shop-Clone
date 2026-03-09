e7beeab872a3f853811d597438801fd5616cac1f
cassielha <cassiel.ha@syncsoftvn.com>
2026-03-09 14:09:59 +0700

    feat: implement initial application structure including layout, home page, and core UI components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
+import './assets/css/tailwind_output.css'
+import './assets/css/style.css';
+import './assets/css/scroll_top.css';
+import './assets/css/badge.css';
+import './assets/css/categories.css';
+import './assets/css/header_web.css';
+import './assets/css/custom_slider.css';
+import './assets/css/slider.css';
+import './assets/css/feature.css';
+import './assets/css/article.css';
+import './assets/css/accordion.css';
+import './assets/css/carousel_arrow.css';
+import './assets/css/footer.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
